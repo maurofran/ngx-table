ae36cb62c8c724c53abe83c8bde459e4eae8254e
Mauro Franceschini <mauro.franceschini@gmail.com>
2021-12-15 18:24:46 +0100

    Migrated from karma to Jest

--- projects/ngx-table/src/lib/ngx-table.module.ts
+++ projects/ngx-table/src/lib/ngx-table.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { NgxTableComponent } from './ngx-table.component';
-
-
 
 @NgModule({
   declarations: [
-    NgxTableComponent
   ],
   imports: [
   ],
   exports: [
-    NgxTableComponent
   ]
 })
 export class NgxTableModule { }
--- projects/ngx-table/src/public-api.ts
+++ projects/ngx-table/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-table
  */
 
-export * from './lib/ngx-table.service';
-export * from './lib/ngx-table.component';
 export * from './lib/ngx-table.module';
